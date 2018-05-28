372b1d3700d6a10703ce802eb983e9f4303c3db0
Daniel Nuñez <danielsistem.ing96@gmail.com>
2018-05-28 12:42:48 -0500

    Crea la rama y añade comentario sobre la gestión del modelo para el usuario

--- projectman/projectman/models.py
+++ projectman/projectman/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User,Group
 from django.db import models
 
 # Create your models here.
+# Hacer uno solo donde este la variable is_model
+class Project_Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user.groups.add(grupo)
+
+class Developer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
