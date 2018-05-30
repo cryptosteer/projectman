e41166be0dabe6c5120cc786a80d3708347a67e8
jrnp97 <jrnp1997@gmail.com>
2018-05-30 04:28:47 -0500

    R03 - Add custom admin dashboard and forms to create task and projects

--- projectman/project/urls.py
+++ projectman/project/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^projectman/', include('projectman.urls')),
+    url(r'', include('projectman.urls')),
+    url(r'^create/', include('task.urls')),
 ]
--- projectman/task/admin.py
+++ projectman/task/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from task.models import Project, Task
 
+
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Task)
+
+class TaskInline(admin.StackedInline):
+    model = Task
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Project Basic Information', {'fields': ['name', 'description', 'project_man']}),
+        ('Project Time Information', {'fields': ['finish_date']})
+    ]
+    inlines = [TaskInline]
+    list_display = ('name', 'init_date', 'project_man', 'finish_date', 'num_task', 'task_done', 'task_process')
+
+
+admin.site.register(Project, ProjectAdmin)
