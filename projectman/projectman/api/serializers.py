bbfd6df46c5aa86a364adde72cddc6df7e35530a
Fredy Mendoza Vargas <fredymv03@gmail.com>
2018-06-08 04:15:20 -0500

    Agregando comentarios por la api (Corregir los datos de relacion para guardar en los serializer)

--- projectman/projectman/api/serializers.py
+++ projectman/projectman/api/serializers.py
@@ -6,21 +6,25 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = ('name',
-                'description',
-                'requeriments',
-                'costs',
-                'estimated_target_date',
-                'priority',
-                'state',
-                'position',
-                'project',
-                'responsable')
+                  'id',
+                  'description',
+                  'requeriments',
+                  'costs',
+                  'estimated_target_date',
+                  'priority',
+                  'state',
+                  'position',
+                  'project',
+                  'comment',
+                  'responsable')
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Comment
         fields = '__all__'
+
 
 class DeveloperProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -32,15 +36,15 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
         fields = ('title',
-                    'description',
-                    'methodology',
-                    'budget',
-                    'resources',
-                    'time_start_real',
-                    'time_end_real',
-                    'time_start_estimated',
-                    'time_end_estimated',
-                    'position', )
+                  'description',
+                  'methodology',
+                  'budget',
+                  'resources',
+                  'time_start_real',
+                  'time_end_real',
+                  'time_start_estimated',
+                  'time_end_estimated',
+                  'position',)
         read_only_fields = ('project_manager', 'client',)
 
 
@@ -49,11 +53,10 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('pk', 'username', 'first_name', 'last_name',
                   'email', 'is_project_manager', 'is_developer',
-                  'is_client', 'image', )
+                  'is_client', 'image',)
 
 
 class ClientProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ClientProfile
         fields = ('pk', 'user', 'cellphone')
-
--- projectman/static/dist/js/myscripts.js
+++ projectman/static/dist/js/myscripts.js
@@ -48,6 +48,88 @@ $('.datatable-active-small').DataTable({
     "lengthMenu": [1, 3, 5, 10, 15],
     ordering: false
 });
+
+addCommentapi = function (task_id) {
+    $('#modal-comment-api').modal('toggle');
+    console.log(task_id);
+    getTaskById(task_id).then(function (resp) {
+        $('#task_title').text(resp.name)
+        $('#task_id_add_comment_form').val('http://localhost:8000/api/task/' + resp.id + '/');
+    });
+
+    //Ajax-
+    $("#btn_agregar_comment_modal").unbind('click').click(function (event) {
+        console.log(event);
+        event.preventDefault();
+        let data = $("#add_comment_api_form").serialize()
+        console.log(data)
+        $.ajax({
+            type: "POST",
+            url: '/api/comment/',
+            data: data,
+            success: function (response) {
+                $('#modal-comment-api').modal('hide');
+                console.log(response)
+                $.notify({message: 'comentario agregado con exito'},
+                    {
+                        type: 'success',
+                        timer: 2500,
+                        placement: {
+                            from: 'top',
+                            align: 'right'
+                        }
+                    }
+                );
+                // if (table_id) {
+                //     if (table_id === 'tabla') {
+                //         location.reload();
+                //     } else {
+                //         const urlHref = location.href + " #" + table_id + ">*";
+                //         $("#" + table_id).load(urlHref, function () {
+                //             if ($('#' + table_id).dataTable() != null) {
+                //                 $('#' + table_id).dataTable().fnDestroy();
+                //             }
+                //             $('#' + table_id).DataTable({
+                //                 responsive: true,
+                //                 language: {
+                //                     "url": url_dataTable
+                //                 }
+                //             });
+                //         });
+                //     }
+                //     // Notificacion
+                //     $.notify({message: message + ' fue eliminada con éxito'},
+                //         {
+                //             type: 'success',
+                //             timer: 2500,
+                //             placement: {
+                //                 from: 'top',
+                //                 align: 'right'
+                //             }
+                //         }
+                //     );
+                // }
+            },
+            error: function (response) {
+                console.error(response);
+                $.notify({message: response},
+                    {
+                        type: 'error',
+                        timer: 2500,
+                        placement: {
+                            from: 'top',
+                            align: 'right'
+                        }
+                    }
+                );
+                $('#modal-comment-api').modal('toggle');
+            }
+        });
+    });
+
+}
+
+
 deleteModalForm = function (url, message, table_id = null, name = '', event) {
     console.log(event);
     $("#form-eliminar").attr("action", url);
@@ -65,9 +147,9 @@ deleteModalForm = function (url, message, table_id = null, name = '', event) {
             success: function (response) {
                 $('#modal-eliminar').modal('hide');
                 if (table_id) {
-                    if(table_id==='tabla'){
+                    if (table_id === 'tabla') {
                         location.reload();
-                    }else{
+                    } else {
                         const urlHref = location.href + " #" + table_id + ">*";
                         $("#" + table_id).load(urlHref, function () {
                             if ($('#' + table_id).dataTable() != null) {
@@ -103,3 +185,10 @@ deleteModalForm = function (url, message, table_id = null, name = '', event) {
 };
 
 // });
+
+function getTaskById(id) {
+    return $.ajax({
+        type: "GET",
+        url: '/api/task/' + id + '/'
+    });
+}
